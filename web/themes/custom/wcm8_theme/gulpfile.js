bf38e180147fcba832f1725760757b43b8b41a32
M Miller <miller.2676@gmail.com>
2019-05-15 16:55:19 -0700

    configure css tasks

--- web/themes/custom/wcm8_theme/gulpfile.js
+++ web/themes/custom/wcm8_theme/gulpfile.js
@@ -1,15 +1,15 @@
 // Load plugins.
-// const autoprefixer = require('autoprefixer');
-// const browsersync = require('browser-sync').create();
-// const cssnano = require('cssnano');
+const autoprefixer = require('autoprefixer');
+const browsersync = require('browser-sync').create();
+const cssnano = require('cssnano');
 const del = require('del');
 const gulp = require('gulp');
-// const plumber = require('gulp-plumber');
-// const postcss = require('gulp-postcss');
+const plumber = require('gulp-plumber');
+const postcss = require('gulp-postcss');
 // const run = require('gulp-run');
-// const sass = require('gulp-sass');
-// const sassGlob = require('gulp-sass-glob');
-// const sourcemaps = require('gulp-sourcemaps');
+const sass = require('gulp-sass');
+const sassGlob = require('gulp-sass-glob');
+const sourcemaps = require('gulp-sourcemaps');
 // const uglify = require('gulp-uglify');
 
 // Configuration.
@@ -26,15 +26,11 @@ config.pl = {
     'libraries/it-osu-ds/.gitignore',
   ],
 };
-
-// config.sass = {
-//   srcFiles: config.baseDirectory + '/style.scss',
-//   watchFiles: [
-//     config.baseDirectory + '/style.scss',
-//     config.baseDirectory + '/**/*.scss',
-//   ],
-//   destDir: 'components/css',
-// };
+config.sass = {
+  srcFiles: 'sass/main.scss',
+  watchFiles: ['sass/main.scss', 'sass/**/*.scss'],
+  destDir: 'css',
+};
 
 // Delete unnecessary PL files.
 function cleanPl(done) {
@@ -42,4 +38,20 @@ function cleanPl(done) {
   done();
 }
 
+// CSS task.
+function css(done) {
+  return gulp
+    .src(config.sass.srcFiles)
+    .pipe(plumber())
+    .pipe(sourcemaps.init())
+    .pipe(sassGlob())
+    .pipe(sass())
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(config.sass.destDir))
+    .pipe(browsersync.stream());
+  done();
+}
+
 exports.cleanPl = cleanPl;
+exports.css = css;
